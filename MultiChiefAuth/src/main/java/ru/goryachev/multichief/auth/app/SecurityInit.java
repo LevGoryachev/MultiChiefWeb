8452d8984886b5b964abc7133e8304c0e6511809
LevGoryachev <l.goryachev@inbox.ru>
2021-06-07 17:03:00 +0300

    auth: change libs to spring-boot-starter-security

--- MultiChiefAuth/src/main/java/ru/goryachev/multichief/auth/app/SecurityInit.java
+++
@@ -1,8 +0,0 @@
-package ru.goryachev.multichief.auth.app;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
-
-
