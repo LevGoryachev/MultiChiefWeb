28e4c8c7bdef64f4cc1792bc2a946ccac5f09cfc
LevGoryachev <l.goryachev@inbox.ru>
2021-06-07 09:17:00 +0300

    Base structure: frontend and auth

---
+++ MultiChiefAuth/src/main/java/ru/goryachev/multichief/auth/app/AppInit.java
@@ -0,0 +1,16 @@
+package ru.goryachev.multichief.auth.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "ru.goryachev.multichief")
+public class AppInit extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder appBuilder) {
+        return super.configure(appBuilder);
+    }
+}
---
+++ MultiChiefAuth/src/main/java/ru/goryachev/multichief/auth/app/SecurityInit.java
@@ -0,0 +1,8 @@
+package ru.goryachev.multichief.auth.app;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
+
+
