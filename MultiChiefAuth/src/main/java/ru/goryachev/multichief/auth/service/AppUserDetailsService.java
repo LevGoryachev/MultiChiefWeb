fdb6318ecf3311a42eb4a33b68a666669880d6b7
LevGoryachev <l.goryachev@inbox.ru>
2021-06-18 17:09:00 +0300

    auth: AppUserDetailsService (loadByUserName())

--- MultiChiefAuth/src/main/java/ru/goryachev/multichief/auth/service/AppUserDetailsService.java
+++ MultiChiefAuth/src/main/java/ru/goryachev/multichief/auth/service/AppUserDetailsService.java
@@ -1,13 +1,24 @@
 package ru.goryachev.multichief.auth.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.goryachev.multichief.auth.entity.AppUser;
+import ru.goryachev.multichief.auth.repository.AppUserRepository;
 
 public class AppUserDetailsService implements UserDetailsService {
 
+    private final AppUserRepository appUserRepository;
+
+    @Autowired
+    public AppUserDetailsService (AppUserRepository appUserRepository){
+        this.appUserRepository = appUserRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User does not exists"));
         return null;
     }
 }
