280cc159aa27a003a228e79efa7023b43c825a2b
LevGoryachev <l.goryachev@inbox.ru>
2022-01-28 16:31:00 +0300

    Create React Next App VERCEL

--- MultiChiefAuth/src/main/java/ru/goryachev/multichief/auth/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.goryachev.multichief.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.goryachev.multichief.auth.entity.Permission;
-
-@Repository
-public interface PermissionRepository extends JpaRepository <Permission, Long> {
-}
